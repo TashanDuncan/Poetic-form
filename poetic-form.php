c758697193f146b6668fc50cf55663da94b10896
Tashan Duncan <tashanduncan1994@gmail.com>
2022-02-21 17:19:06 +0000

    add index and meta info

---
+++ poetic-form.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Poetic Form
+ * Description: Poetic Form plugin
+ * Author: Tashan Duncan
+ * Author URI: https://tashanducan.com
+ * Version: 1.0.0
+ * Text Domain: poetic-form
+ * 
+ */
